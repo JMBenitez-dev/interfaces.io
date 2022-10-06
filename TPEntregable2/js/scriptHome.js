06f597a37aecf92c7f36e37f7a3c77302c6fc0bb
Rocio Correa <correarocy@gmail.com>
2022-10-06 01:19:15 -0300

    Agreagando Open Menu y detalles de botones

--- TPEntregable2/js/scriptHome.js
+++ TPEntregable2/js/scriptHome.js
@@ -5,16 +5,16 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the <button> element that closes the modal
+var close = document.getElementById("close");
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+// When the user clicks on <button> (x), close the modal
+close.onclick = function() {
   modal.style.display = "none";
 }
 
